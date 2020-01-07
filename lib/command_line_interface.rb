d74a7b36cd3017f8a0658269961af1d416c61da0
Cole Rau <colevrau@gmail.com>
2020-01-06 16:37:22 -0800

    Done.

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -4,5 +4,7 @@ end
 
 def get_character_from_user
   puts "please enter a character name"
-  # use gets to capture the user's input. This method should return that input, downcased.
+  character_name = gets.chomp.to_s
+
 end
+
