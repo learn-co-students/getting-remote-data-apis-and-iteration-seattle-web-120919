0853e546db2277f57ab7d30d39b9d93471fd2228
github username <github email address>
2020-04-16 12:44:02 -0700

    Done.

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -1,25 +1,36 @@
 require 'rest-client'
 require 'JSON'
 require 'pry'
+require 'net/http'
+require 'open-uri'
 
 # return array of hashes, each hash = movie info
 def get_character_movies_from_api(character)
-  # API request
+
+  # API request for all_characters
   response_string = RestClient.get('https://swapi.dev/api/people')
   response_hash = JSON.parse(response_string)
+  character_data = response_hash[:results]
 
-  # iterate to find collection of `films` for the given `character`
-  response_hash.map{|char| char.films}
-  # collect film API urls, make a web request to each URL to get the info
-  #  for that film
+  film_urls = character_data.each do |char| 
+    if char[:name] === "Luke Skywalker"
+        puts char[:films]
+    end
+  end 
+
+  # char = character_data.map{|char| char[:name] == character}
+  # # iterate to find collection of `films` for the given `character`
+  # film_urls = character_data[:films]
+
+  # collect film API urls, make a web request to each URL to get film details
+  film_details = film_urls.map{|url| JSON.parse(RestClient.get(url)) }
+  
   # return value of this method should be collection of info about each film.
   #  i.e. an array of hashes in which each hash reps a given film
   # this collection will be the argument given to `print_movies`
-  #  and that method will do some nice presentation stuff like puts out a list
-  #  of movies by title. Have a play around with the puts with other info about a given film.
 end
 
-# some iteration magic and puts out the movies in a nice list
+# some iteration magic, puts out the movies by title in a nice list
 def print_movies(films)
   films.map{|film| film.title}
 end
@@ -28,8 +39,3 @@ def show_character_movies(character)
   films = get_character_movies_from_api(character)
   print_movies(films)
 end
-
-## BONUS
-
-# that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
-# can you split it up into helper methods?
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,9 @@
 def welcome
-  puts "welcome! may the fourth be with you"
+  puts "welcome! may the fourth be with you!!"
 end
 
 def get_character_from_user
   puts "please enter a character name"
-  # capture the user's input, downcased.
+  gets.chomp
+  # capture the user's input, return input downcased.
 end
